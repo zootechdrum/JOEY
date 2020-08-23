ba5e6e9c9c5bd7acf8ff6824ee60d812f5ccab4a
Joey Reyes <joeyr1984@gmail.com>
2020-08-22 22:14:51 -0700

    Updates

---
+++ precache-manifest.db1c21cb276e0ed7c4144050bcc5f2c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0533e48ba6015d9007ed771efa421297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c14a66d80ee7b56f40a1",
+    "url": "/static/css/main.94b7f0d5.chunk.css"
+  },
+  {
+    "revision": "97bff82770136cd3b673",
+    "url": "/static/js/2.096f3d82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.096f3d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14a66d80ee7b56f40a1",
+    "url": "/static/js/main.6ba38feb.chunk.js"
+  },
+  {
+    "revision": "8a204d8a5195133a5ab5",
+    "url": "/static/js/runtime-main.896285ec.js"
+  },
+  {
+    "revision": "20e0e4f3edb22aac6a17de2c4da14d6f",
+    "url": "/static/media/desk.20e0e4f3.jpg"
+  },
+  {
+    "revision": "231cddca194d1dd07b58ee616aaa7878",
+    "url": "/static/media/dome.231cddca.svg"
+  }
+]);
