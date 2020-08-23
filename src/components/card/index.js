41e84fe08eb70cb669a1aff741b36424e54c2ce5
Joey Reyes <joeyr1984@gmail.com>
2020-08-23 15:57:04 -0700

    styling fixes

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,20 +1,28 @@
 import React from "react";
 
+const styles = {
+  cardStyle: {
+    marginBottom: 10
+
+  }
+}
+
 
 function Card(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          </ul>
-      </div>
+    <div className="col-4">
+      <div style={styles.cardStyle} className="card">
+        <div className="img-container">
+          <a href={props.url}><img className="card-img-top" alt={props.name} src={props.image} /></a>
+        </div>
+        <div className="card-body">
 
+          <strong className="card-title">{props.name}</strong>
+          <br />
+          <a href={props.repo} class="btn btn-primary">View Repo</a>
+        </div>
+
+      </div>
     </div>
   );
 }
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,26 +1,28 @@
 import React, { Component } from "react";
 import Card from "../components/card";
-import project from "../project.json";
+import projects from "../project.json";
 import Wrapper from "../components/Wrapper";
 
-let card = [];
+//let card = [];
 
 class Work extends Component {
   render() {
     return (
-      <>
-        <Wrapper>
+      <div class="row">
+        
           
-          {this.state.project.map(project => (
+          {projects.map(project => (
             <Card
               key={project.id}
               name={project.name}
               image={project.image}
+              repo={project.repo}
+              url={project.url}
             />
           ))}
-          {card}
-        </Wrapper>
-      </>
+          {/* {card} */}
+        
+      </div>
     );
   }
 }
